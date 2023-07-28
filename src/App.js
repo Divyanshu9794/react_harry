2fe76b74819930de176c554d7474a0b9fdc87518
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-07-28 23:51:47 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import TextForm from './components/TextForm'
 import Navbar from './components/Navbar';
 function App() {
   return (
     <>
    
-<Navbar title="TextUtils" abouttext="About textUtils"/>
+<Navbar title="TextUtils" />
+<div className="container" my-3>
+
+<TextForm heading="Enter the text to analyse below"/>
+</div>
 {/* <Navbar /> */}
   </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function TextForm() {
+export default function TextForm(props) {
+  const [text, setText] = useState('Enter text here');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div>
-      <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+      <h1>
+        {props.heading} - {text}
+      </h1>
+      <div className="mb-3">
+        {/* Add the onChange event handler to update the 'text' state */}
+        <textarea
+          className="form-control" id="myBox" rows="8"value={text}
+          onChange={handleTextChange}
+        ></textarea>
+      </div>
+      <button className="btn btn-primary">Convert To upper case</button>
     </div>
-  )
+  );
 }
