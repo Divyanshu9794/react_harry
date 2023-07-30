52adc6345d5988012af97d2866122a8d0e50511b
Divyanshu9794 <sdivyanshu352@gmail.com>
2023-07-30 18:05:18 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,20 +6,25 @@ export default function TextForm(props) {
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
+  const handleupclick = () =>{
+    let newtext = text.toUpperCase();
 
+    setText(newtext)
+  }
+ 
   return (
     <div>
       <h1>
         {props.heading} - {text}
       </h1>
       <div className="mb-3">
-        {/* Add the onChange event handler to update the 'text' state */}
+      
         <textarea
           className="form-control" id="myBox" rows="8"value={text}
           onChange={handleTextChange}
         ></textarea>
       </div>
-      <button className="btn btn-primary">Convert To upper case</button>
+      <button className="btn btn-primary" onClick={handleupclick}>Convert To upper case</button>
     </div>
   );
 }
